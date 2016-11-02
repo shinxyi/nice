96d099e7674e6a68430a344ef32f172ba0e78519
Will Wright <wawwright@gmail.com>
2016-11-02 11:44:28 -0500

    missed these files last commit

--- Wright_Will/Assignments/jsFund/deckofcards.js
+++ Wright_Will/Assignments/jsFund/deckofcards.js
@@ -14,62 +14,51 @@ Deck.prototype={
             arr[rand] = temp
         }
     },
+    //places card on top of cards
+    addCard: function(card){
+        this.cards.push(card)
+        return this
+    },
+    //fills cards deck with all cards in order
     build: function(){
         this.cards = []
         addSuite = (suite) => {
             hex = "1F0" +  suite + 0
             dec = parseInt(hex,16)
             for (let i = 1; i <= 13; i++){
-                card = String.fromCodePoint(dec + i)
+                var card = String.fromCodePoint(dec + i)
                 this.cards.push(card)
             }
         }
         ["A","B","C","D"].forEach(addSuite)
-        // ["A","B","C","D"].forEach(function(suite){
-        //     hex = "1F0" +  suite + 0
-        //     dec = parseInt(hex,16)
-        //     for (let i = 1; i <= 13; i++){
-        //         card = String.fromCodePoint(dec + i)
-        //         this.cards.push(card)
-        //     }
-        // },this)
     },
     deal: function(){
         return this.cards.pop()
     }
 }
-
+// Player Constructor
 function Player(name="johndoe"){
     this.name = name
     this.hand = []
 }
+
 Player.prototype = {
     drawCard: function(deckObj,n=1){
         for (let i = 0; i < n; i++) {
             this.hand.push(deckObj.deal())
         }
+    },
+    discardCard: function(cardIndex=0){
+        this.hand = this.hand.slice(0,cardIndex).concat(this.hand.slice(cardIndex+1))
     }
 }
 
 
 
-deck = new Deck()
-player = new Player("fred")
-// deck.shuffle(2)
-// deck.reset()
-console.log(deck.cards.length);
-console.log(deck.cards);
-// player.drawCard(deck)
-// console.log(console.log(deck.cards.length));
-// console.log(player.hand);
-
-
-
-// suites2 = ["A","B","C","D"]
-// cards = [1,2,3,4,5,6,7,8,9,10]
-// var str = String.fromCodePoint(127139)
+// deck = new Deck()
+// player = new Player("fred")
 //
-// console.log(str);
-// // console.log('	🂱')
-// // "U+1F0A1"
-// console.log(parseInt("1fd0f",16));
+// console.log(deck.cards.length);
+
+// exports.Player = Player
+// exports.Deck = Deck
--- Wright_Will/Assignments/jsFund/test.js
+++ Wright_Will/Assignments/jsFund/test.js
@@ -1,42 +1,5 @@
-// a = 0
-//
-// console.log(a);
-// console.log(a);
-// console.log(a)
-// console.log(a++);
-// console.log(a);
-
-
-//
-// arr = [
-// "foo",
-// "bar",
-// "hello",
-// "world"
-// ]
-// phrasesArr= []
-// current_word = 3
-// current_char = 3
-// console.log(arr[current_word]);
-// console.log(arr[current_word][current_char]);
-// console.log("hello"[current_char]);
-// arr = ["foo"]
-// console.log(arr[0][1]);
-
-// ["A","B","C","D"].forEach(function(n){
-//     console.log(n);
-// })
-
-
-// obj = {}
-//
-// obj.letters= function(){
-//     ["A","B","C","D"].forEach(function(n){
-//         console.log(n);
-//     })
-// }
-//
-// obj.letters()
-
-
-console.log("+&nbsp;");
+arr = [1,2,3,4,5,6,7]
+a = arr.slice(0,1)
+b = arr.slice(1 + 1)
+console.log(a);
+console.log(b);
