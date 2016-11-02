680e2e7cd13795dca6c75cfae4b96abda2524c31
Will Wright <wawwright@gmail.com>
2016-11-02 14:44:13 -0500

    finished fib again...

--- Wright_Will/Assignments/jsFund/fibinacci/fibinacciClosure.js
+++ Wright_Will/Assignments/jsFund/fibinacci/fibinacciClosure.js
@@ -12,19 +12,3 @@ fibCounter() // should console.log "2"
 fibCounter() // should console.log "3"
 fibCounter() // should console.log "5"
 fibCounter() // should console.log "8"
-
-function fibA() {
-  arr=[0,1]
-  return () => {
-    console.log(arr)
-    arr.push(arr[arr.length-1] + arr[arr.length-2])
-    return arr
-  }
-}
-
-fn = fibA()
-fn()
-fn()
-fn()
-fn()
-fn()
