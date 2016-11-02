56a27c904a77c66ce708085e195a17d41baa7e95
aphillips-810 <aphillipsbaseball@gmail.com>
2016-11-02 13:19:42 -0500

    Assignments 11-2

---
+++ Anthony Phillips/createObject3.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+function VehicleConstructor(name, numOfWheels, numOfPassengers, speed) {
+        var vin = Math.random();
+        this.name = name;
+        this.numOfWheels = numOfWheels;
+        this.numOfPassengers = numOfPassengers;
+        this.speed = speed;
+        var distance_traveled = 0;
+        var update_distance_traveled = function () { distance_traveled += this.speed }
+        this.makeNoise = function () { console.log("Cling-cling") };
+
+        this.move = function () { update_distance_traveled(); makeNoise(); };
+        this.checkMiles = function () { console.log(this.distance_traveled); }
+        this.showVin = function () { console.log(vin) };
+
+        return this
+    }
+
+
+    var i = new VehicleConstructor('Bike', 2, 1, 200);
+    i.makeNoise = function () { console.log("Ring-ring") };
+    console.log(i.makeNoise())
+
+    var s = new VehicleConstructor('Sedan', 4, 1, 200);
+    i.makeNoise = function () { console.log("Honk-honk") };
+    console.log(s.makeNoise())
+
+    var b = new VehicleConstructor('Bus', 6, 1, 200);
+    b.addPassengers = function (arg) { this.numOfPassengers += arg }
+    b.addPassengers(5)
+    console.log(b.numOfPassengers)
+
