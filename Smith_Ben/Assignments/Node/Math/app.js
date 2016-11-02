46cd606a88da4dbb408dabcf9f4e1ef771418e3f
Ben <benfloydsmith@gmail.com>
2016-11-02 16:30:40 -0500

    Node Work

---
+++ Smith_Ben/Assignments/Node/Math/app.js
@@ -0,0 +1,7 @@
+
+
+var math_module = require('./mathlib');
+math_module.add(2,3);
+math_module.multiply(3,5);
+math_module.square(5);
+math_module.random(10,100);
---
+++ Smith_Ben/Assignments/Node/Node_Modules/app.js
@@ -0,0 +1,14 @@
+// var http = require('http');
+
+// var fs = require('fs');
+
+// var server = http.createServer(function (request, response){
+// 	console.log('client request URL: ', request.url);
+
+var my_module = require('./my_module');
+my_module.greet();
+my_module.add(5,6);
+
+// });
+
+
