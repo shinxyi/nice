882e9d9eacec64c0215a965497c99f1ab442a037
Will Wright <wawwright@gmail.com>
2016-11-03 11:34:28 -0500

    made hello express

---
+++ Wright_Will/Assignments/express_basics/server.js
@@ -0,0 +1,13 @@
+
+express = require('express')
+app = express()
+
+
+app.get('/',function(request,response){
+    response.send("hello Express")
+})
+
+// app.listen(8000,function () {
+//     console.log(String(app.get));
+// })
+console.log(String(app.listen));
